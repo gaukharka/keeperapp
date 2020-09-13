8f262d2bb518d50b7ee8a244b1b774baa44e151b
gaukharka <gaukhar.kashaganova@gmail.com>
2020-09-13 15:45:01 +0200

    Updates

---
+++ precache-manifest.db94df533c1f59f09e2aaa4150bdd5a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ec69320cf189efb42bca2e6b292a29",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "0b1e90e27334848b2d38",
+    "url": "/keeper/static/js/2.90f3b997.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keeper/static/js/2.90f3b997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee9a0e4ad4c8c32f2f6e",
+    "url": "/keeper/static/js/main.eee0d17f.chunk.js"
+  },
+  {
+    "revision": "b0f3c69d11e458d5f477",
+    "url": "/keeper/static/js/runtime-main.3e2bed01.js"
+  }
+]);
